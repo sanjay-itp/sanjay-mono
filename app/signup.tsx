eca907e175688e8c7c5f8a15e1e48b5c19bbed2e
Sanjay Magar <sanjaymagar2007@gmail.com>
2026-07-31 13:57:57 +0545

    new

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,4 +1,5 @@
 import react from 'react';
+import { Button } from '@/components/buttons';
 import {StyleSheet, View, Text} from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,7 +9,7 @@ import { InputField } from '@/components/input';
 const style=StyleSheet.create({
     container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#ffffff"
   },
   top:{
     position: "absolute",
@@ -20,23 +21,65 @@ const style=StyleSheet.create({
     top: 90,
     left: 40,
   },
-    toptext:{ 
-        fontSize: 25,
+    heading:{ 
+        fontSize: 30,
       fontWeight: '600',
       color: "black",
       
     },
-    topbelowtext:{
-        fontSize: 15,
+    subhead:{
+        fontSize: 17,
         color:"#5A8C88",
         paddingTop: 10,
     },
     inputs:{
+        marginTop: 15,
+        borderColor: "#C8EDEA",
+        borderWidth: 2,
+        borderRadius: 10,
+        marginBottom: 15,
+        color: "black",
+        width: "95%",
+    }, 
+    place:{
         position: "absolute",
-        top: 300,
+        top: 250,
         alignSelf: "center",
         width: "90%",
-    }
+        padding: 10,
+        left: 20,
+    },
+    button:{
+    backgroundColor:"#549B96",
+    // paddingVertical: 12,
+    // margin:10,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:"black",
+    borderWidth:1,
+    boxShadow: "0px 6px 16px -3px rgba(0,0,0,0.46)",
+    height:55,
+    alignSelf:'center',
+    width:'90%',
+    marginTop:0,
+    padding: 10,
+    position : "absolute",
+    },
+    buttonTitle: {
+    color: '#FFFFFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+    }, 
+    textblwbtn:{
+    marginTop:10,
+    textAlign:"center",
+    fontSize:14,},
+    logintxt:{
+      color:"#428581",
+    },
+
+    
 })
 
 
@@ -48,15 +91,19 @@ export default function Login() {
             <AntDesign name="left" size={24} color="black" />
         </View>
         <View style={style.topContainer}>
-            <Text style={style.toptext}>Create</Text>
-            <Text style={style.toptext}>Account ✨</Text>
-            <Text style={style.topbelowtext}>Join to start tracking your finances</Text>
+            <Text style={style.heading}>Create</Text>
+            <Text style={style.heading}>Account ✨</Text>
+            <Text style={style.subhead}>Join to start tracking your finances</Text>
         </View>
-        <View style={style.inputs}>
+        <View style={style.place}>
             <InputField placeholder="Jane Doe"  label='Full Name' style={style.inputs}/>
             <InputField placeholder="jane.doe@example.com" label='Email Address' style={style.inputs}/>
             <InputField placeholder="Create a strong password" label='Password' style={style.inputs}/>
         </View>
+        <SafeAreaView>
+            <Button title="Create Accoount" type="primary" style={style.button} textStyle={style.buttonTitle} />
+            <Text style={style.textblwbtn}>Already have an account? <Text style={style.logintxt}>Login</Text></Text>
+        </SafeAreaView>
     </View>
     )
 }
