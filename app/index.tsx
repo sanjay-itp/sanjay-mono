d42ba7120e1674d76924c5fd744d77d6961363e2
Sanjay Magar <sanjaymagar2007@gmail.com>
2026-07-31 11:43:56 +0545

    hello

---
+++ app/index.tsx
@@ -0,0 +1,67 @@
+import { Text, StyleSheet, View } from 'react-native';
+import { Image } from "expo-image";
+import Background from '@/assets/svg/background.svg';
+import { Button } from '@/components/buttons';
+import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"#fff"
+    },
+  logo: {
+    position: "absolute",
+    top: 100,
+    right:10,
+    left:0,
+    bottom:0
+  },
+  button:{
+    backgroundColor:"#549B96",
+    // paddingVertical: 12,
+    // margin:10,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor:"black",
+    borderWidth:1,
+    boxShadow: "0px 6px 16px -3px rgba(0,0,0,0.46)",
+    height:55,
+    alignSelf:'center',
+    width:'90%',
+    marginTop:50,
+
+  },
+  buttonTitle: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  textblwbtn:{
+    marginTop:10,
+    textAlign:"center",
+    fontSize:14,},
+    logintxt:{
+      color:"#428581",
+    }
+});
+
+
+
+export default function Index() {
+  const router = useRouter();
+  return (
+    <View>
+    <View>
+      <Background/>
+       <Image
+        source={require('@/assets/svg/model.png')}
+        style={styles.logo} />
+    </View>
+      <SafeAreaView>
+          <Button title="Get Started" type="primary" style={styles.button} textStyle={styles.buttonTitle} onPress={() => router.push('/tabs/profile')}/>
+            <Text style={styles.textblwbtn}>Already have an account? <Text style={styles.logintxt}>Log in</Text></Text>
+      </SafeAreaView>
+    </View>
+  )
+}
---
+++ components/buttons.tsx
@@ -0,0 +1,50 @@
+import { Pressable, PressableProps, StyleProp, StyleSheet, Text, TextStyle, ViewStyle } from "react-native"
+
+type Props = PressableProps & {
+    title: string
+    type: 'primary' | 'secondary' | 'outline' | 'danger' | 'disabled'
+    textStyle?: StyleProp<TextStyle>
+}
+
+const buttonStyles: Record<Props['type'], ViewStyle> = {
+    primary: {
+        backgroundColor: 'lightblue',
+    },
+    secondary: {
+        backgroundColor: 'green',
+    },
+    outline: {
+        borderWidth: 1,
+        borderColor: 'blue',
+    },
+    danger: {
+        backgroundColor: 'red',
+    },
+    disabled: {
+        backgroundColor: 'gray',
+    },
+} as const
+
+const styles = StyleSheet.create({
+    common: {
+        paddingHorizontal: 12,
+        paddingVertical: 12,
+        margin:10,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
+})
+
+export function Button({ title, type = 'primary', style, textStyle, ...props }: Props) {
+    return (
+        <Pressable style={[styles.common, buttonStyles[type], style]as StyleProp<ViewStyle>} {...props}>
+            <Text style={[styles.text, textStyle]}>{title}</Text>
+        </Pressable>
+    )
+}
